f401e8631ef07cce511a57c7215e2871b6636766
Ad.popov <sahek12332.93@gmail.com>
2017-06-30 17:39:25 +0300

    Caclulator

--- src/main/java/jet/calc/CalculatorApp.java
+++ src/main/java/jet/calc/CalculatorApp.java
@@ -1,20 +1,18 @@
 package jet.calc;
 
 
+import static jet.calc.Calculator.executeCommand;
+
 public class CalculatorApp {
-    public static int memory = 0;
-    public static int currentMemory = 0;
 
     public static void main(String[] args) {
 
-        System.out.println(Calculator.add(1,2));
-        System.out.println(Calculator.sub(1,2));
-        Calculator.saveToMemory();
-        System.out.println(Calculator.div(1,2));
-        System.out.println(Calculator.mult(1,2));
-        System.out.println(Calculator.showMemory());
-        Calculator.addMemory();
-        System.out.println(Calculator.showMemory());
-
+        for (int i = 0; i < args.length; i += 3){
+            String command = args[i];
+            double a = Double.parseDouble(args[i+1]);
+            double b = Double.parseDouble(args[i+2]);
+            System.out.println(executeCommand(command, a, b));
+        }
     }
 }
+
